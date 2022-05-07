909bb8c3b8fd1252edbf36295e2c0ceadb489881
imzuyel <zuyel76@gmail.com>
2022-05-07 13:17:51 +0600

    Code man Portfolio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Header sector
+let nav = document.querySelector(".navbar");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+}
+// nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (e) {
+  e.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
